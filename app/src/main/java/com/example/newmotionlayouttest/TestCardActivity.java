1d4c435e2d3e68b6d3348b4338d95412b4cf94d2
bam4wo <43362974+bam4wo@users.noreply.github.com>
2021-09-09 15:35:04 +0800

    0909 password card test

---
+++ app/src/main/java/com/example/newmotionlayouttest/TestCardActivity.java
@@ -0,0 +1,14 @@
+package com.example.newmotionlayouttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_card);
+    }
+}
